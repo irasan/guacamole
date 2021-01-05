3d4274fb1f4d1afe5b7a8b0db5ee9d722945e52d
irasan <iryna.sanzhara@gmail.com>
2021-01-05 18:36:01 +0000

    Finish index.html

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,23 @@
-$(document).on('click','.toggleMusic',function(){
+$(document).on('click', '.toggleMusic', function () {
     icon = $(this).find("i");
-        icon.toggleClass('fa-volume-off fa-volume-mute');
+    icon.toggleClass('fa-volume-off fa-volume-mute');
 });
+
+function game() {
+    var avocado = "assets/images/beetle.png";
+    var smashed = "assets/images/target.png";
+    setTimeout(displayAvocado, 2000);
+    clearTimeout();
+};
+
+function displayAvocado() {
+    setInterval(document.getElementById("start").src = avocado, 2000);
+    document.getElementById('played').onclick = function () {
+        displayScore();
+    }
+}
+
+function displayScore() {
+    document.getElementById("start").src = smashed;
+
+};
