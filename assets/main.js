37e3741cbef871417571dee9803fcf94b7dbff5a
irasan <iryna.sanzhara@gmail.com>
2021-01-06 17:50:04 +0000

    Add grid of the game field in game.html

--- assets/main.js
+++ assets/main.js
@@ -3,21 +3,3 @@ $(document).on('click', '.toggleMusic', function () {
     icon.toggleClass('fa-volume-off fa-volume-mute');
 });
 
-function game() {
-    var avocado = "assets/images/beetle.png";
-    var smashed = "assets/images/target.png";
-    setTimeout(displayAvocado, 2000);
-    clearTimeout();
-};
-
-function displayAvocado() {
-    setInterval(document.getElementById("start").src = avocado, 2000);
-    document.getElementById('played').onclick = function () {
-        displayScore();
-    }
-}
-
-function displayScore() {
-    document.getElementById("start").src = smashed;
-
-};
