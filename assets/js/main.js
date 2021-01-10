c8ddc148f68a79c2f86cce85dfda07e29b63fada
irasan <iryna.sanzhara@gmail.com>
2021-01-10 16:27:31 +0000

    Fix bugs with smashed not turning back to avocados. Add sound effects when smashing them

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
-
-$(document).on('click', '.toggleMusic', function () {
+$(document).on('click', '.toggleMusicButton', function () {
     icon = $(this).find("i");
-    icon.toggleClass('fa-volume-off fa-volume-mute');
+    icon.toggleClass('fa-volume-mute fa-volume-off');
 });
 
 //variables
 var avocados = document.querySelectorAll('.avocado');
 var bowls = document.querySelectorAll('.bowl');
 var timeUp = false;
-// const highestScore = document.getElementById('#highestScore');
+var smashSound = new sound("assets/audio/smash.wav");
+var gameMusic = new sound("assets/audio/gameMusic.wav");
+var isPlaying = false;
+// var highestScore = document.getElementById('#highestScore');
 
 // function to get a random avocado appear in a hole
 function randomAvocado(avocados) {
@@ -25,8 +27,10 @@ function randomTime(min, max) {
 
 // function that makes an avocado to pop up
 function popUp() {
+    // gameMusic.muted = true;
+    gameMusic.play();
     var activeAvocado = randomAvocado(avocados);
-    var popUpTime = randomTime(1000, 2500);
+    var popUpTime = randomTime(1000, 1800);
 
     activeAvocado.classList.remove('down');
     console.log("continue");
@@ -39,6 +43,21 @@ function popUp() {
     }, popUpTime);
 }
 
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
 function startGame() {
     console.log("start");
     timeUp = false;
@@ -47,10 +66,28 @@ function startGame() {
     setTimeout(() => timeUp = true, 15000); //show random moles for 15 seconds
 }
 
-function smash(e) {
-    this.classList.add("smashed");
-    console.log("rotate");
-    //debugger;
+function smash() {
+    this.src = "assets/images/smash.png";
+    console.log(this.src);
+    smashSound.play();
+    setTimeout(() => {
+        this.src = "assets/images/avocado1.png";
+    }, 750);
+    console.log(this.src);
 }
 
+// function to play and pause game music
+function togglePlay() {
+  isPlaying ? gameMusic.pause() : gameMusic.play();
+};
+
+gameMusic.onplaying = function() {
+  isPlaying = true;
+};
+gameMusic.onpause = function() {
+  isPlaying = false;
+};
+
+//event listeners
 avocados.forEach((item) => item.addEventListener("click", smash));
+document.getElementsByID("playMusic").addEventListener("click", togglePlay);
